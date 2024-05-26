7c47d1eb103202335912530fc809464a3e0dd363
HECTOR ULISES MENDIOLA MORALES <110561031+Ulises-Mendiola@users.noreply.github.com>
2024-05-26 15:06:33 -0700

    Delete SHIKA_PAGINA directory

--- SHIKA_PAGINA/menu.js
+++
@@ -1,8 +0,0 @@
-// Función para mostrar/ocultar el menú
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.classList.toggle("show");
-}
-
-// Asignar evento clic al botón de menú
-document.getElementById("menu-btn").addEventListener("click", toggleMenu);
